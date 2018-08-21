99f42e72dba0eea9b5abbd599f12b20428809e74
X901 <im.x901@gmail.com>
2018-08-21 21:14:24 +0300

    Initial Commit

---
+++ RateApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RateApp
+//
+//  Created by X901 on 21/08/2018.
+//  Copyright © 2018 X901. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
